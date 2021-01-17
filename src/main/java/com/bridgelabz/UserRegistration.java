5ee95043d9001d5e58528913056872c7e152516b
su-mahesh <maheshkangude@gmail.com>
2021-01-17 23:33:14 +0530

    ADDED: first name test case

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    private String firstLastNamePattern = "[A-Z][a-z]{2}[a-z]*";
+
+    public boolean firstName(String firstName){
+            return Pattern.matches(firstLastNamePattern, firstName);
+        }
+    }
+
---
+++ src/test/java/com/bridgelabz/FirstNameTest.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.*;
+
+@RunWith(Parameterized.class)
+    public class FirstNameTest{
+        UserRegistration userRegistration = new UserRegistration();
+
+        private String firstName;
+        private boolean expectedResult;
+
+        public FirstNameTest(String firstName, boolean expectedResult){
+            super();
+            this.firstName = firstName;
+            this.expectedResult = expectedResult;
+        }
+
+        @Parameterized.Parameters
+        public static Collection input(){
+            return Arrays.asList(new Object[][] {{"Mahesh", true}, {"mahesh", false}, {"m", false}, {"MAHESh", false}, {"Mahesh1", false}});
+        }
+
+        @Test
+        public void testFirstNameTest(){
+            Assert.assertEquals(expectedResult, userRegistration.firstName(firstName));
+        }
+    }
+
+
+
+
